943079c991146957048fc1c38ab44b4448606340
Davide Pessi <davide1.pessi@mail.polimi.it>
2024-08-18 18:51:11 +0200

    aggiunto il quadrato modello

--- MeshLoader.cpp
+++ MeshLoader.cpp
@@ -323,9 +323,9 @@ class MeshLoader : public BaseProject {
 		initialBackgroundColor = {0.0f, 0.005f, 0.0f, 1.0f};
 		
 		// Descriptor pool sizes
-		DPSZs.uniformBlocksInPool = 106;//aumento di 2
-		DPSZs.texturesInPool = 150;//aumentato di 3
-		DPSZs.setsInPool = 59;//aumento di 1
+		DPSZs.uniformBlocksInPool = 108;//aumento di 2
+		DPSZs.texturesInPool = 153;//aumentato di 3
+		DPSZs.setsInPool = 60;//aumento di 1
 		
 		Ar = (float)windowWidth / (float)windowHeight;
 	}
